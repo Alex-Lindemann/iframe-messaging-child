c026ff717276337b8ae1ca5ed17e7b7297216f54
Alex Lindemann <AlexDeadmanLindemann@gmail.com>
2022-09-21 09:16:15 -0500

    Testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import SimpleBottomNavigation from './components/Navigation';
+import Body from './components/Body';
+
+window.addEventListener("message", (event) => {
+  if (event?.data?.source !== 'react-devtools-bridge' && event?.data?.source !== 'react-devtools-detector' && event?.data?.source !== 'react-devtools-content-script' && event?.data?.type !== 'webpackWarnings') {
+
+    console.log("messaged", event)
+  }
+}, false);
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <Routes>
+          <Route
+            path="/"
+            element={<><SimpleBottomNavigation /><Body /></>}
+          />
+
+          <Route
+            path="/iframe"
+            element={<><Body /></>}
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,49 @@
+import { useSearchParams } from "react-router-dom";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from "react";
+
+function getReturnUrlBase() {
+    if (window.location.origin === "http://localhost:3001") {
+        return 'http://localhost:3000';
+    }
+    return 'https://'
+}
+
+export default function Body() {
+    const [data, setData] = useState({ fieldOne: '' })
+    let [searchParams] = useSearchParams();
+    let params = {};
+
+    for (const entry of searchParams.entries()) {
+        const [param, value] = entry;
+        params[param] = value;
+    }
+
+    const sendToParent = () => {
+        if (window.location.pathname === '/') {
+            window.location.href = `${getReturnUrlBase()}?fieldValues=${encodeURIComponent(JSON.stringify(data))}`
+        } else {
+            window.parent.postMessage({ type: 'message-to-parent', data: { fieldValues: data } }, '*');
+        }
+    }
+
+    return (
+        <>
+            Incoming Data: {JSON.stringify(params)}
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField id="filled-basic" label="Field One" variant="filled" onChange={(e) => setData({ fieldOne: e.target.value })} />
+                <br />
+                <Button variant="outlined" onClick={sendToParent}>Send To App One</Button>
+            </Box>
+        </>
+    )
+}
